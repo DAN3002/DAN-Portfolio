b82960b8706611b0dd0fdaaf7b0871d9120f2479
DAN3002 <dinhanh300229@gmail.com>
2022-12-01 21:11:47 +0700

    add Section Component

---
+++ src/components/Section.jsx
@@ -0,0 +1,14 @@
+function Section({ title, children}) {
+	return (
+		<section id='experience'>
+			<div className='container'>
+				<h2 className='section-title wow fadeInUp'>{title}</h2>
+				<div className='spacer' data-height={60} />
+
+				{children}
+			</div>
+		</section>
+	);
+}
+
+export default Section;

8b775047d25cadfc46255fe2cf2bfd4abbd613ea
DAN3002 <dinhanh300229@gmail.com>
2023-01-08 17:56:14 +0700

    Add DesktopPortfolioFilter~

--- src/components/work/WorkSection.jsx
+++ src/components/work/WorkSection.jsx
@@ -1,23 +1,12 @@
+import DesktopPortfolioFilter from './DesktopPortfolioFilter';
+
 function WorkSection() {
 	const { PUBLIC_URL } = process.env;
 
 	return (
 		<div>
-			{/* portfolio filter (desktop) */}
-			<ul className="portfolio-filter list-inline wow fadeInUp">
-				<li className="current list-inline-item" data-filter="*">
-					All
-				</li>
-				<li className="list-inline-item" data-filter=".webapp">
-					Webapp
-				</li>
-				<li className="list-inline-item" data-filter=".ai">
-					Artificial Intelligence
-				</li>
-				<li className="list-inline-item" data-filter=".other">
-					Other
-				</li>
-			</ul>
+			<DesktopPortfolioFilter />
+
 			{/* portfolio filter (mobile) */}
 			<div className="pf-filter-wrapper">
 				<select className="portfolio-filter-mobile">

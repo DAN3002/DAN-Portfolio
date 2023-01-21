4dc47e835d52ab54b2321562dacdb3f9716f40b8
DAN3002 <dinhanh300229@gmail.com>
2023-01-21 23:47:01 +0700

    Fix loading bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React, { useEffect } from 'react';
+
 import PreLoader from './components/utils/PreLoader';
 import MobileHeader from './components/header/MobileHeader';
 import DesktopHeader from './components/header/DesktopHeader';
@@ -8,10 +10,12 @@ import Main from './components/Main';
 function App() {
 	const { PUBLIC_URL } = process.env;
 
-	const script = document.createElement('script');
-	script.src = `${PUBLIC_URL}/js/custom.js`;
-	script.async = true;
-	document.body.appendChild(script);
+	useEffect(() => {
+		const script = document.createElement('script');
+		script.src = `${PUBLIC_URL}/js/custom.js`;
+		script.async = true;
+		document.body.appendChild(script);
+	}, []);
 
 	return (
 		<>

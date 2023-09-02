c67f4b292af0d4e52e36c5395773c99d56da1b0f
DAN3002 <dinhanh300229@gmail.com>
2023-09-03 00:43:34 +0700

    Render items as probs

--- src/components/achieve/AchieveSection.jsx
+++ src/components/achieve/AchieveSection.jsx
@@ -9,6 +9,36 @@ function AchieveSection() {
 
 	const { achievements } = data;
 
+	const items = achievements.map((item, index) => (
+		<div className="blog-item rounded bg-dark shadow-light wow fadeIn">
+			<a
+				href={`#small-dialog-achieve-${index}`}
+				className="work-content"
+			>
+				<div className="thumb">
+					<span className="category">{item.category}</span>
+					<img
+						src={`${PUBLIC_URL}${item.thumb}`}
+						alt={item.name}
+						style={{
+							width: '330px',
+							height: '268px',
+						}}
+					/>
+				</div>
+				<div className="details">
+					<h4 className="my-0 title">{item.name}</h4>
+					<ul className="list-inline meta mb-0 mt-2">
+						<li className="list-inline-item">
+							{item.date}
+						</li>
+						<li className="list-inline-item">{item.prize}</li>
+					</ul>
+				</div>
+			</a>
+		</div>
+	));
+
 	return (
 		<div className="row blog-wrapper">
 			{/* {achievements.map((item, index) => (
@@ -68,7 +98,9 @@ function AchieveSection() {
 					</div>
 				</div>
 			))} */}
-			<Carousel />
+			<Carousel
+				items={items}
+			/>
 		</div>
 	);
 }
--- src/components/utils/Carousel.jsx
+++ src/components/utils/Carousel.jsx
@@ -2,46 +2,25 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { useEffect } from 'react';
 
-function Carousel() {
+function Carousel({
+	items,
+}) {
 	useEffect(() => {
 		$('.testimonials-wrapper').slick({
 			dots: true,
 			arrows: false,
-			autoplay: true,
-			autoplaySpeed: 3000,
+			infinite: true,
+			speed: 200,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			// autoplay: true,
+			// autoplaySpeed: 3000,
 		});
 	}, []);
 
 	return (
 		<div className="testimonials-wrapper">
-			<div className="testimonial-item text-center mx-auto">
-				<div className="thumb mb-3 mx-auto">
-					<img decoding="async" src="wp-content/uploads/sites/8/2021/01/avatar-1-3.svg" alt />
-				</div>
-				<h4 className="mt-3 mb-0">John Doe</h4>
-				<span className="subtitle">Product designer at Dribbble</span>
-				<div className="bg-white padding-30 shadow-dark rounded triangle-top position-relative mt-4">
-					<p className="mb-0">
-						I enjoy working with the theme and learn so much.
-						You guys make the process fun and interesting.
-						Good luck! 👍
-					</p>
-				</div>
-			</div>
-			<div className="testimonial-item text-center mx-auto">
-				<div className="thumb mb-3 mx-auto">
-					<img decoding="async" src="wp-content/uploads/sites/8/2021/01/avatar-3-1.svg" alt />
-				</div>
-				<h4 className="mt-3 mb-0">John Doe</h4>
-				<span className="subtitle">Product designer at Dribbble</span>
-				<div className="bg-white padding-30 shadow-dark rounded triangle-top position-relative mt-4">
-					<p className="mb-0">
-						I enjoy working with the theme and learn so much.
-						You guys make the process fun and interesting.
-						Good luck! 🔥
-					</p>
-				</div>
-			</div>
+			{items}
 		</div>
 	);
 }

2f2111320a17bd6c99591576673b7544b5829a28
DAN3002 <dinhanh300229@gmail.com>
2023-09-05 01:30:45 +0700

    Show modal when add slick

--- src/components/achieve/AchieveSection.jsx
+++ src/components/achieve/AchieveSection.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import parse from 'html-react-parser';
 
 import data from '../../data/data';
@@ -41,63 +40,34 @@ function AchieveSection() {
 
 	return (
 		<div className="row blog-wrapper">
-			{/* {achievements.map((item, index) => (
-				<div className="col-md-4">
-					<div className="blog-item rounded bg-dark shadow-light wow fadeIn">
-						<a
-							href={`#small-dialog-achieve-${index}`}
-							className="work-content"
-						>
-							<div className="thumb">
-								<span className="category">{item.category}</span>
-								<img
-									src={`${PUBLIC_URL}${item.thumb}`}
-									alt={item.name}
-									style={{
-										width: '330px',
-										height: '268px',
-									}}
-								/>
-							</div>
-							<div className="details">
-								<h4 className="my-0 title">{item.name}</h4>
-								<ul className="list-inline meta mb-0 mt-2">
-									<li className="list-inline-item">
-										{item.date}
-									</li>
-									<li className="list-inline-item">{item.prize}</li>
-								</ul>
-							</div>
-						</a>
-						<div
-							id={`small-dialog-achieve-${index}`}
-							className="white-popup zoom-anim-dialog mfp-hide"
-						>
-							<img
-								src={`${PUBLIC_URL}${item.image}`}
-								alt={item.name}
-							/>
-							<div className="spacer" data-height={5} />
-							{item.text.map((text) => (
-								<p>{parse(text)}</p>
-							))}
+			{achievements.map((item, index) => (
+				<div
+					id={`small-dialog-achieve-${index}`}
+					className="white-popup zoom-anim-dialog mfp-hide"
+				>
+					<img
+						src={`${PUBLIC_URL}${item.image}`}
+						alt={item.name}
+					/>
+					<div className="spacer" data-height={5} />
+					{item.text.map((text) => (
+						<p>{parse(text)}</p>
+					))}
 
-							<div className="spacer" data-height={10} />
-							<div style={{ marginLeft: 10 }}>
-								<ul>
-									{item.link.map((el) => (
-										<li>
-											<a href={el.url}>
-												{el.text}
-											</a>
-										</li>
-									))}
-								</ul>
-							</div>
-						</div>
+					<div className="spacer" data-height={10} />
+					<div style={{ marginLeft: 10 }}>
+						<ul>
+							{item.link.map((el) => (
+								<li>
+									<a href={el.url}>
+										{el.text}
+									</a>
+								</li>
+							))}
+						</ul>
 					</div>
 				</div>
-			))} */}
+			))}
 			<Carousel
 				items={items}
 			/>
